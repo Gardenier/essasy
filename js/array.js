94331ba72a53e9cd0a14b1338a4619e7544ffdb1
gardenia <gardenia@gardeniadeMacBook-Pro.local>
2019-05-26 22:33:03 +0800

    update

---
+++ js/array.js
@@ -0,0 +1,11 @@
+var array1 = [1, 3, 2, 6, 9, 8, 7]
+var array2 = [4, 5]
+var array3 = []
+/**concat() 不会改变原数组 */
+// console.log(array1.concat(array2), array1)
+/**join() */
+// console.log(array2.join(','), array2)
+/**pop() 改变原数组，返回最后一个元素，空数组返回undefined*/
+// console.log(array2.pop(), array2, array3.pop())
+/**push() 改变原数组，返回新数组长度*/
+// console.log(array2.push(6), array2)
